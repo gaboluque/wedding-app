e0a31a00ce7cbf4fa820b0fda26e790c47f0f542
Gabriel Luque <gluquemiranda@gmail.com>
2024-09-16 08:52:21 -0500

    chore: remove inicio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import { Timeline } from "@/components/Timeline";
-
-
 export default function Home() {
   return (
     <main className={`home`}>
-      <h5 className="text-center mb-5">Nuestra historia</h5>
-      <Timeline />
+      Inicio
+      TODO
     </main>
   )
 }
