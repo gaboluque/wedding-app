5e15b70325ce89714f04cfda3cc5a6f0a493a0c5
Gabriel Luque <gluquemiranda@gmail.com>
2024-09-25 09:44:13 -0500

    chore: truncate to 100% progress

--- src/utils/productUtils.ts
+++ src/utils/productUtils.ts
@@ -2,6 +2,7 @@ import { IProduct } from "@/models/Product";
 
 
 export const productProgress = (product: IProduct): string => {
-  // Product amount vs product progress in percentage
-  return Number(((product.progress || 0) / product.totalAmount) * 100).toFixed(0);
+  // Product amount vs product progress in percentage, truncate to 100%
+  const percentage =  Number(((product.progress || 0) / product.totalAmount) * 100)
+  return percentage > 100 ? "100" : percentage.toFixed(0);
 }
